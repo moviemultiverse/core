4c6052218162762aa56551b852aa82ac08728e76
Saurabh Saraswat <saurabh45215@gmail.com>
2023-06-22 23:34:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,108 @@
+
+const { google } = require('googleapis');
+const fs = require('fs');
+var express = require('express'); 
+var app = express();
+async function createFilePermission(authClient, fileId, emailAddress, role) {
+  try {
+    const drive = google.drive({ version: 'v3', auth: authClient });
+
+    const permission = {
+      role: role,
+      type: 'user',
+      emailAddress: emailAddress
+    };
+
+    const response = await drive.permissions.create({
+      fileId: fileId,
+      requestBody: permission,
+      fields: 'id'
+    });
+     console.log(`Permission created with ID: ${response.data.id}`);
+     return response.data.id;
+  } catch (error) {
+    console.error('Error creating permission:', error);
+  }
+}
+async function deleteFilePermission(fileId, permissionId) {
+  try {
+    // Load the service account credentials from JSON key file
+    const credentials = require('./drive-download-389811-b229f2e27ed8.json');
+
+
+    // Configure the Google API client with the service account credentials
+    const auth = new google.auth.GoogleAuth({
+      credentials,
+      scopes: ['https://www.googleapis.com/auth/drive'],
+    });
+
+    // Create a Google Drive client
+    const drive = google.drive({ version: 'v3', auth });
+
+    // Delete the file permission
+    await drive.permissions.delete({
+      fileId,
+      permissionId,
+    });
+
+    console.log(`Permission ${permissionId} deleted successfully.`);
+  } catch (error) {
+    console.error('Error deleting permission:', error);
+  }
+}
+        
+  
+app.listen(3000);
+app.get('/', (req, res) => {
+res.sendfile( 'index.html');
+});
+app.get('/api', function(req, res) {
+  const user_id = req.query.id; 
+  const file_id = req.query.fileid;
+
+  try {
+    // Load the service account credentials
+    const credentials = require('./drive-download-389811-b229f2e27ed8.json');
+
+    // Create an auth client using the service account credentials
+    const authClient = new google.auth.GoogleAuth({
+      credentials: credentials,
+      scopes: ['https://www.googleapis.com/auth/drive']
+    });
+
+    // Authorize the client
+    const auth = await authClient.getClient();
+
+    // Specify the file ID, email address, and role for the permission
+    const fileId = file_id;
+    const emailAddress = user_id;
+    const role = 'writer';
+
+    // Create the file permission
+    await createFilePermission(auth, fileId, emailAddress, role)
+  .then(response => {
+    // Wait for 5 minutes (300,000 milliseconds) and then call the second function with the response from the first function
+    setTimeout(() =>  deleteFilePermission(fileId, response), 300000);
+  })
+  .catch(error => {
+    console.error('Error occurred:', error);
+  });
+
+
+      } catch (error) {
+    console.error('Error:', error);
+  }
+
+
+  res.send({
+    'user_id': user_id
+  });
+});
+app.post("/", async (req, res) => {
+ // req.query.messsage
+ 
+console.log(req.body);
+ console.log(req);
+  res.json(req);
+ alert(req);
+});
