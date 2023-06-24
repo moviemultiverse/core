0dd224f55bd6c96e2e8f9f8a3fdacca3946cd48e
Saurabh Saraswat <saurabh45215@gmail.com>
2023-06-24 18:40:15 +0530

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,53 @@
+
+const keyFile = 'drive-download-389811-b229f2e27ed8.json'; // Path to your service account credentials file
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+
+// Load the service account credentials
+const credentials = require(keyFile);
+
+// Create a JWT client
+const auth = new google.auth.JWT(
+  credentials.client_email,
+  null,
+  credentials.private_key,
+  SCOPES
+);
+auth.authorize((err, tokens) => {
+  if (err) {
+    console.error('Authentication failed:', err);
+    return;
+  }
+
+  console.log('Authentication successful!');
+  uploadFile(auth);
+});
+
+function uploadFile(auth) {
+  const drive = google.drive({ version: 'v3', auth });
+
+  const fileMetadata = {
+    name: 'VID20230624183754.mp4', // Name of the file in Google Drive
+  };
+
+  const media = {
+    mimeType: 'video/mp4',
+    body: fs.createReadStream('VID20230624183754.mp4'), // Path to the local MP4 file
+  };
+
+  drive.files.create(
+    {
+      resource: fileMetadata,
+      media: media,
+      fields: 'id',
+    },
+    (err, res) => {
+      if (err) {
+        console.error('Upload failed:', err);
+        return;
+      }
+
+      console.log('File uploaded successfully!');
+      console.log('File ID:', res.data.id);
+    }
+  );
+}
