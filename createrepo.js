e32d263fcb012efcdc9948dc7361ee8ebcd2d03f
Saurabh Saraswat <saurabh45215@gmail.com>
2023-07-08 19:44:02 +0530

    Update createrepo.js

--- createrepo.js
+++ createrepo.js
@@ -1,5 +1,24 @@
 const axios = require('axios');
 const fs = require('fs');
+function getpublickey(){
+  const owner = "ss0809";
+const repo = "my-new-repo";
+const token = "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";
+fetch(`https://api.github.com/repos/${owner}/${repo}/actions/secrets/public-key`, {
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    const publicKey = data.key_id;
+    console.log(publicKey);
+  })
+  .catch(error => {
+    console.error("Error retrieving environment public key:", error);
+  });
+
+}
 function replacer(filePath,searchWord,replacement){
 // Read the file contents
 fs.readFile(filePath, 'utf8', (err, data) => {
@@ -108,5 +127,5 @@ const createRepository = async () => {
   }
 };
 
-createRepository();
+getpublickey();
 
