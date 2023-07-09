ff77a339e3e6637e94888278b2552d4a5643e6e3
Saurabh Saraswat <saurabh45215@gmail.com>
2023-07-09 19:10:10 +0530

    Update createchannel.js

--- createchannel.js
+++ createchannel.js
@@ -1,15 +1,12 @@
 const { v4: uuidv4 } = require('uuid');
-
-// Generate a UUID
-const channel_Id = uuidv4();
-
-console.log('Channel ID:', channel_Id);
-
 const { google } = require('googleapis');
 const { JWT } = require('google-auth-library');
 
 // Load the service account private key JSON file
 const privatekey = require('./drive-download-389811-b229f2e27ed8.json');
+
+// Generate a UUID for the channel
+const channelId = uuidv4();
 
 // Authenticate using the service account
 const jwtClient = new JWT({
@@ -22,12 +19,11 @@ async function createDriveNotificationChannel() {
   const drive = google.drive({ version: 'v3', auth: jwtClient });
 
   // Define the notification channel parameters
-  const channelId =channel_Id;
   const resourceId = '13cPqUdKzJM4vuYX-GD0YvhtZgvZNa1aF';
   const notificationUrl = 'https://discord.com/api/webhooks/1127586462888632442/rZ0jAcTLZPjTATiVcgqySR8nD81SBdqTS-Dvam9TA51NTcJdRlk9-7ZOjFajPt_C_zFY';
 
   // Create the channel
-  const response = await drive.files.watch({
+  const response = await drive.changes.watch({
     fileId: resourceId,
     requestBody: {
       id: channelId,
@@ -40,4 +36,4 @@ async function createDriveNotificationChannel() {
 }
 
 createDriveNotificationChannel().catch(console.error);
-    
+
