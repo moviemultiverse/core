55ac035b8feaaa3b997f75addf838ed30a422d1f
Saurabh Saraswat <saurabh45215@gmail.com>
2023-07-10 18:23:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -447,7 +447,7 @@ app.post("/post", async (req, res) => {
  console.log(req.headers);
   });
 try {
-  await sendDiscordWebhook('https://discord.com/api/webhooks/1127586462888632442/rZ0jAcTLZPjTATiVcgqySR8nD81SBdqTS-Dvam9TA51NTcJdRlk9-7ZOjFajPt_C_zFY', 'Hello, Discord!');
+  sendDiscordWebhook('https://discord.com/api/webhooks/1127586462888632442/rZ0jAcTLZPjTATiVcgqySR8nD81SBdqTS-Dvam9TA51NTcJdRlk9-7ZOjFajPt_C_zFY', 'Hello, Discord!');
   } catch (error)
   {
     console.error(error);
