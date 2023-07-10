78dece6cd4f19d0e704c72acd27570a338c94485
Saurabh Saraswat <saurabh45215@gmail.com>
2023-07-10 18:04:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -445,10 +445,5 @@ async function sendDiscordWebhook(webhookURL, message) {
 app.post("/post", async (req, res) => {
  console.log("posted");
  console.log(req.headers);
-  const webhookURL = 'https://discord.com/api/webhooks/1127586462888632442/rZ0jAcTLZPjTATiVcgqySR8nD81SBdqTS-Dvam9TA51NTcJdRlk9-7ZOjFajPt_C_zFY';
-const message = 'req.headers';
-
-await sendDiscordWebhook(webhookURL, message);
-
-
+  
 });
