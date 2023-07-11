708490e7ab2b8ed6882104a72e122b01e69db5ed
Saurabh Saraswat <saurabh45215@gmail.com>
2023-07-11 12:40:42 +0530

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,11 +1,11 @@
-split -b 1m input.mp4 output_ --additional-suffix=.txt
+split -b 20m input.mp4 output_ --additional-suffix=.txt
 
 count=0
 folder_count=1
 mkdir -p folder$folder_count
 
 for file in output_*; do
-  if [ $count -eq 10 ]; then
+  if [ $count -eq 50 ]; then
     count=0
     folder_count=$((folder_count + 1))
     mkdir -p folder$folder_count
