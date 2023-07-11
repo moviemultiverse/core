647f1117ac2ab23b49273b51a496632004379a6a
Saurabh Saraswat <saurabh45215@gmail.com>
2023-07-11 16:22:31 +0530

    Update gtod.py

--- gtod.py
+++ gtod.py
@@ -2,15 +2,6 @@ import os
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-import requests
-
-def call_url_with_params(url, params):
-    response = requests.get(url, params=params)
-    # Check the response status code
-    if response.status_code == requests.codes.ok:
-        return response.text
-    else:
-        return f"Request failed with status code {response.status_code}"
 
 def main():
     credentials = service_account.Credentials.from_service_account_file('client_secrets.json', scopes=['https://www.googleapis.com/auth/drive.file'])
@@ -21,20 +12,24 @@ def main():
     file_path = 'randomfile.mp4'  # Replace with the path to your file
 
     file_metadata = {'name': os.path.basename(file_path), 'parents': [folder_id]}
-    media = MediaFileUpload(file_path)
+    media = MediaFileUpload(file_path, resumable=True)
 
-    file = drive_service.files().create(
-        body=file_metadata,
-        media_body=media,
-        fields='id'
-    ).execute()
+    try:
+        file = drive_service.files().create(
+            body=file_metadata,
+            media_body=media,
+            fields='id'
+        ).execute()
 
-    print(f'File uploaded. ID: {file["id"]}')
-    url = 'https://google-06xl.onrender.com/driveupdate'
-    params = {'name': file_path, 'id': file["id"]}
-    response_text = call_url_with_params(url, params)
-    print(response_text)
+        print(f'File uploaded. ID: {file["id"]}')
+        url = 'https://google-06xl.onrender.com/driveupdate'
+        params = {'name': file_path, 'id': file["id"]}
+        response_text = call_url_with_params(url, params)
+        print(response_text)
+
+    except Exception as e:
+        print(f'Error occurred during file upload: {str(e)}')
 
 if __name__ == '__main__':
     main()
-    
+        
