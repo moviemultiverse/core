ca23d3a1cd3e930f1a84b7b5b98ce70e446542d3
Saurabh Saraswat <saurabh45215@gmail.com>
2023-07-11 12:41:57 +0530

    Update gtod.sh

--- gtod.sh
+++ gtod.sh
@@ -1,2 +1,18 @@
+#!/bin/bash
+
+# List of folders to move contents from
+folders=("folder1" "folder2" "folder3" "folder4")
+
+# Iterate through each folder
+for folder in "${folders[@]}"; do
+  # Check if the folder exists
+  if [ -d "$folder" ]; then
+    # Move contents of the folder to the root directory
+    mv "$folder"/* ./
+  else
+    echo "Folder '$folder' does not exist."
+  fi
+done
+
 cat *.txt > randomfile.mp4
 echo "cool it ended";
