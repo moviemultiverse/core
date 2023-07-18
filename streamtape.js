8dab6e14c8ebd1150716672c53841c3f061598ed
SS0809 <saurabh45215@gmail.com>
2023-07-18 15:49:16 +0530

    Update streamtape.js

--- streamtape.js
+++ streamtape.js
@@ -1,28 +1,32 @@
-  const axios = require('axios');
+const axios = require('axios');
+
 function getLinkByName() {
+  const apiUrl = 'https://api.streamtape.com/file/listfolder?login=f65b540c475b9b7d4da8&key=268XaKDBLqTZ2kg';
+  const targetFileName = "Fast_iX.mp4";
 
-const apiUrl = 'https://api.streamtape.com/file/listfolder?login=f65b540c475b9b7d4da8&key=268XaKDBLqTZ2kg';
-const targetFileName = "Fast_iX.mp4";
-
-axios.get(apiUrl)
-  .then(response => {
-    // Handle the response data here
-    console.log('Response data:', response.data);
-
-  const data = JSON.parse(response.data); // Parse the JSON data into a JavaScript object
-  const files = data.result.files;
-  for (const file of files) {
-    if (file.name === targetName) {
-      return file.link;
-    }
-  }
-  })
-  .catch(error => {
-    // Handle errors here
-    console.error('Error:', error.message);
-  });
-  return null; // Return null if the file with the given name is not found
+  return axios.get(apiUrl)
+    .then(response => {
+      // Handle the response data here
+      const data = response.data; // No need to parse, as Axios has already done it
+      const files = data.result.files;
+      for (const file of files) {
+        if (file.name === targetFileName) { // Fixed variable name from 'targetName' to 'targetFileName'
+          return file.link;
+        }
+      }
+      return null; // Return null if the file with the given name is not found
+    })
+    .catch(error => {
+      // Handle errors here
+      console.error('Error:', error.message);
+      return null; // Return null in case of an error
+    });
 }
 
-const link = getLinkByName();
-console.log(link);
+getLinkByName()
+  .then(link => {
+    console.log(link);
+  })
+  .catch(error => {
+    console.error('Error:', error.message);
+  });
