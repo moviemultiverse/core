dd1bf5d25f90035f191b5e32b5dfe15d6d4d7e36
SS08090 <techicalsolutions68@gmail.com>
2023-07-27 11:57:53 +0530

    maintenance error graphql

--- graphql.js
+++ graphql.js
@@ -1,30 +1,86 @@
-const express = require("express");
-const { createHandler } = require( 'graphql-http/lib/use/express');
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve: () => 'world',
-      },
-      hello1: {
-        type: GraphQLString,
-        resolve: () => 'world1',
-      },
-    },
-  }),
-});
-const app = express();
-app.use(
-  "/graphql",
-  createHandler({
-    schema: schema,
-  })
-);
-app.listen(4000, () => {
-  console.log("Running a GraphQL API server at localhost:4000/graphql");
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { GraphQLJSON, GraphQLJSONObject } = require('graphql-type-json');
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const pool = new Pool({
+  host: 'satao.db.elephantsql.com',
+  port: 5432,
+  database: 'iywyfbqc',
+  user: 'iywyfbqc',
+  password: 'qAGx55jepOzWXVmB2IZxn-F-rulL3zRR'
 });
 
-//curl -X POST   -H "Content-Type: application/json"   -d '{"query": "{ hello ,hello1 }"}'   http://localhost:4000/graphql
+const typeDefs = `
+scalar JSON
+scalar JSONObject
+
+type Movie {
+  movie_name: String!
+  doodstream_code: String
+  streamtape_code: String
+  img_data: JSONObject  
+}
+
+type Query {
+  movie(movie_name: String!): Movie
+  allMovieNames: [String!]!
+}
+
+`;
+
+const resolvers = {
+  Query: {
+    movie: async (parent, { movie_name }) => {
+      try {
+        const query = 'SELECT * FROM moviedata WHERE movie_name = $1';
+        const values = [movie_name];
+        const result = await pool.query(query, values);
+        return result.rows[0];
+      } catch (error) {
+        console.error('Error executing query', error);
+        throw new Error('Error retrieving movie');
+      }
+    },
+    allMovieNames: async () => {
+      try {
+        const query = 'SELECT movie_name FROM moviedata';
+        const result = await pool.query(query);
+        return result.rows.map(row => row.movie_name);
+      } catch (error) {
+        console.error('Error executing query', error);
+        throw new Error('Error retrieving movie names');
+      }
+    },
+  },
+};
+
+const app = express();
+const httpServer = http.createServer(app);
+
+// Set up Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+// Start the Apollo Server before using expressMiddleware
+async function startServer() {
+  await server.start();
+  app.use(
+    cors(),
+    bodyParser.json(),
+    expressMiddleware(server),
+  );
+
+  httpServer.listen({ port: 4000 }, () => {
+    console.log(`🚀 Server ready at http://localhost:4000`);
+  });
+}
+
+startServer().catch(err => console.error(err));
