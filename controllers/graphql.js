735374c54454f98364250b02726fbb900ec7c3a2
ss0809 <saurabh45215@gmail.com>
2024-02-19 13:27:47 +0530

    AWS EC2 compatible

--- controllers/graphql.js
+++ controllers/graphql.js
@@ -1,3 +1,15 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
+
+
 const typeDefs = `
   scalar JSON
   scalar JSONObject
